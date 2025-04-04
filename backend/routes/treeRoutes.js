cc7d60b6682e6bcaec6bb4508a7e5321067e680c
Francois Lerosier <francois.lerosier@gmail.com>
2025-04-04 15:31:20 +0200

    try5

--- backend/routes/treeRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var treeController_1 = require("../controllers/treeController");
-var router = (0, express_1.Router)();
-// Routes for tree-related operations
-router.get('/', treeController_1.default.getAllTrees); // Route to get all trees
-router.get('/:id', treeController_1.default.getTreeById); // Route to get a tree by ID
-exports.default = router;
