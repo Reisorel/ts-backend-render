150782b4e47b774ded4db8cbbbf8dd76b04031ae
Francois Lerosier <francois.lerosier@gmail.com>
2025-04-08 16:31:15 +0200

    trying to push render

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -1,5 +1,5 @@
 // 👇 change manuellement ici entre 'dev' ou 'prod'
-const ENV = 'dev'; // ou 'prod'
+const ENV = 'prod' as 'dev' | 'prod'; // ou 'dev'
 
 const API_BASE_URL = ENV === 'dev'
   ? import.meta.env.VITE_API_URL_DEV
